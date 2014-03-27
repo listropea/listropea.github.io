8949814da4e3270b276599a928853bd3f32c47c0
Elisabeth Tropea <lis@bittersweetcretive.com>
2014-03-26 20:08:21 -0400

    initial import

---
+++ main.js
@@ -0,0 +1,21 @@
+/* psuedo code:
+
+Fixed Header Img: This will keep the 'PIES + PETALS' image bar stuck to the top of the page at all times.
+When scrolling keep img fixed at top of page
+
+Modal Windows: This will hide the content in menu items and then content show when clicked
+Hide menu items content
+When clicking menu items show hidden content
+When clicking off of menu item area close out of window
+
+Gallery/Blog: This will make images in gallery show blog title when hovered over
+When menu item 'see' is clicked open new page
+View photos in grid
+When photo is hovered over pull title of blog to show up over image
+When clicking on photo take to blog post
+
+*/
+
+
+
+
